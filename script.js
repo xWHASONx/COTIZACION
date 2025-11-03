dfdfb88b49ffa1da543f1f0e9cccc880cfb239ea
xWHASONx <d4ni3lg0m3zm4rtin3z@gmail.com>
2025-11-03 13:51:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,11 @@
 // --- 1. CONFIGURACIÓN GLOBAL ---
 const ADVISORS = {
     'katherine_rueda': { name: 'Katherine Rueda', photoUrl: 'https://i.imgur.com/21GKFPV.png', defaultWhatsapp: '573249450254' },
-    'yonder_gonzalez': { name: 'Yonder Gonzalez', photoUrl: 'https://i.imgur.com/IPWYtIU.jpeg', defaultWhatsapp: '573508917512' },
-    'juan_madrid': { name: 'Juan David Madrid', photoUrl: 'https://i.imgur.com/Oanbam8.jpeg', defaultWhatsapp: '573145352537' },
+    'Daniela_Ardila ': { name: 'Daniela Ardila', photoUrl: 'https://i.imgur.com/08PClCm.jpeg', defaultWhatsapp: '573213349780' },
     'valentina_herrera': { name: 'Valentina Herrera', photoUrl: 'https://i.imgur.com/MHGZASr.png', defaultWhatsapp: '573184188621' },
     'maryory_gonzalez': { name: 'Maryory Gonzalez', photoUrl: 'https://i.imgur.com/G9nHKlg.jpeg', defaultWhatsapp: '573189150322' },
-    'margareth_saucedo': { name: 'Margareth Saucedo', photoUrl: 'https://i.imgur.com/peBTIni.jpeg', defaultWhatsapp: '573502687290' },
     'karen_giraldo': { name: 'Karen Giraldo', photoUrl: 'https://i.imgur.com/Amn1Wtb.jpeg', defaultWhatsapp: '573216839241' },
-    'sofia_giraldo': { name: 'Sofia Giraldo', photoUrl: 'https://i.imgur.com/GMOLJxa.jpeg', defaultWhatsapp: '573113854751' },
-    'nicol_tamayo': { name: 'Nicol Tamayo', photoUrl: 'https://i.imgur.com/7pg3HvF.jpeg', defaultWhatsapp: '' },
-    'daniela_cardona': { name: 'Daniela Cardona', photoUrl: 'https://i.imgur.com/dwnDmHy.png', defaultWhatsapp: '' },
+    'Alejandro_Jimes': { name: 'Alejandro Jaimes', photoUrl: 'https://i.imgur.com/1RuwHv0.jpeg', defaultWhatsapp: '573012033720' },
     'sarah_esteves': { name: 'Sarah George Esteves', photoUrl: 'https://i.imgur.com/mt1UKBS.jpeg', defaultWhatsapp: '573226817507' },
     'Carolina_Muñoz': { name: 'Carolina Muñoz', photoUrl: 'https://i.imgur.com/sXnamkJ.jpeg', defaultWhatsapp: '573113855927' }
 };
